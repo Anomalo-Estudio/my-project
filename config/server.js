6bc02725a19a0b5ea915259b5582f4a3a95552e4
seha13 <out_sherreraa@independencia.com.mx>
2020-10-12 16:02:53 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '96bd479281fa23cf61ea5ba496eab24e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '687ec8cd-a6bd-4ef1-9c04-03933c8eb9f9'
+};
